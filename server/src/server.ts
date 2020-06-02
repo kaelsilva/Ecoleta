56b317e808678b0eca978cd1a0ef3e975dea581c
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-06-02 16:16:24 -0300

    Adicionando arquivos do segundo dia.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-  ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '../', 'uploads')));
 
 app.listen(3333);
